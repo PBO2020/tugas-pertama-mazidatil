424fe5985a08cf6dc38f7c5c4a69937fc32401b7
mazidatil <60373234+mazidatil@users.noreply.github.com>
2020-01-28 11:35:18 +0700

    Tugas  1
    Kumpul tugas 1

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        secondclass mz = new secondclass();
+        mz.setAngka1(5);
+        mz.setAngka2(8);
+        System.out.println("Angka 1  = " + mz.getAngka1());
+        System.out.println("Angka 2  = " + mz.getAngka2());
+        System.out.println("Hasilnya = " + mz.getHasil1());
+        System.out.println("Hasilnya = " + mz.getHasil2());
+        System.out.println("Hasilnya = " + mz.getHasil3());
+    }
+}
---
+++ secondclass.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class secondclass {
+
+
+
+        int Angka1;
+
+        public int getAngka1() {
+            return Angka1;
+        }
+
+        public void setAngka1(int Angka1) {
+            this.Angka1=Angka1;
+        }
+
+        int Angka2;
+
+        public int getAngka2() {
+            return Angka2;
+        }
+
+        public void setAngka2(int Angka2) {
+            this.Angka2 = Angka2;
+        }
+        int Hasil1;
+
+        public int getHasil1() {
+            Hasil1=Angka1+Angka2;
+            return Hasil1;
+        }
+        int Hasil2;
+
+        public int getHasil2() {
+            Hasil2=Angka1*Angka2;
+            return Hasil2;
+        }
+        int Hasil3;
+
+        public int getHasil3() {
+            Hasil3=Angka1*Angka2;
+            return Hasil3;
+        }
+
+
+        public void setHasil(int Hasil) {
+            this.Hasil1 = Hasil1;
+        }
+
+
+
+
+    }
+
+
